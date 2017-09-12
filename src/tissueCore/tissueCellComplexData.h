4b7444af119bbbf5fad723f9d33cc360442be80b
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2017-09-12 17:05:58 +0200

    minor changes : use Count naming

--- src/tissueCore/tissueCellComplexData.h
+++ src/tissueCore/tissueCellComplexData.h
@@ -22,19 +22,24 @@ public:
     virtual bool hasElement(int dimension, unsigned long id) const;
 
     virtual unsigned long* elementIds(int dimension) const;
+    virtual unsigned long elementCount(int dimension) const;
 
 //  ///////////////////////////////////////////////////////////////////
 //  Incidence concept
 //  ///////////////////////////////////////////////////////////////////
 
     virtual unsigned long* borders(int dimension, unsigned long id) const;
-    virtual int nbBorders(int dimension, unsigned long id) const;
+    virtual int borderCount(int dimension, unsigned long id) const;
     virtual unsigned long* regions(int dimension, unsigned long id) const;
-    virtual int nbRegions(int dimension, unsigned long id) const;
+    virtual int regionCount(int dimension, unsigned long id) const;
 
 // OR
 
     virtual unsigned long* incidentElementIds(int dimension, unsigned long id, int incidenceDimension) const;
-    virtual int nbIncidentElements(int dimension, unsigned long id, int incidenceDimension) const;
+    virtual int incidentElementCount(int dimension, unsigned long id, int incidenceDimension) const;
 
+//  ///////////////////////////////////////////////////////////////////
+//  Neighborhood concept
+//  ///////////////////////////////////////////////////////////////////
+    
 }
