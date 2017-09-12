f7437b0aa807a411a258c2102607bd5675c1d547
Julien Wintz <julien.wintz@inria.fr>
2017-09-12 09:35:29 +0200

    Initial revision.

---
+++ app/tissueGrowthSimulator/main.cpp
@@ -0,0 +1,25 @@
+// Version: $Id$
+//
+//
+
+// Commentary:
+//
+//
+
+// Change Log:
+//
+//
+
+// Code:
+
+#include <QtCore>
+
+int int main(int argc, char *argv[])
+{
+    QCoreApplication application(argc, argv);
+
+    return 0;
+}
+
+//
+// main.cpp ends here
