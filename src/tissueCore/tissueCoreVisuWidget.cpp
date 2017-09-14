206169c9a4551921c6c15441688ddd8dab129207
Jean-Luc Szpyrka <Jean-Luc.Szpyrka@inria.fr>
2017-09-14 17:13:16 +0200

    addCell method added to VisuWidget

--- src/tissueCore/tissueCoreVisuWidget.cpp
+++ src/tissueCore/tissueCoreVisuWidget.cpp
@@ -19,8 +19,24 @@
 #include <dtkComposer/dtkComposerViewController.h>
 #include <QVTKOpenGLWidget.h>
 #include <vtkContextView.h>
-#include <vtkRenderer.h>
 #include <vtkGenericOpenGLRenderWindow.h>
+
+#include "vtkActor.h"
+#include "vtkCellArray.h"
+#include "vtkDoubleArray.h"
+#include "vtkFloatArray.h"
+#include "vtkIntArray.h"
+#include "vtkPointData.h"
+#include "vtkPoints.h"
+#include "vtkPolyData.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkRenderer.h"
+
+
+
+#include "tissueCellComplexData.h"   // use tissueCellComplex in final release
 
 class tissueCoreVisuWidgetPrivate
 {
@@ -28,14 +44,19 @@ public:
     vtkGenericOpenGLRenderWindow *window;
     QVTKOpenGLWidget *widget;
     vtkRenderer *renderer;
+
+public:
+    vtkPolyData *data;
 };
 
 tissueCoreVisuWidget::tissueCoreVisuWidget(QWidget *parent) : QFrame(parent)
 {
     d = new tissueCoreVisuWidgetPrivate;
 
+    d->data = nullptr;
+
     d->renderer = vtkRenderer::New();
-    d->renderer->SetBackground(1, 0, 0);
+    d->renderer->SetBackground(0, 0, 0);
 
     d->window = vtkGenericOpenGLRenderWindow::New();
     d->window->AddRenderer(d->renderer);
@@ -53,8 +74,81 @@ tissueCoreVisuWidget::tissueCoreVisuWidget(QWidget *parent) : QFrame(parent)
 
 tissueCoreVisuWidget::~tissueCoreVisuWidget(void)
 {
+    d->renderer->Delete();
+    d->window->Delete();
     delete d->widget;
     delete d;
+}
+
+void tissueCoreVisuWidget::addCellComplex(tissueCellComplexData &cell)
+{
+    if ( ! d->data ) {
+        dtkWarn() << Q_FUNC_INFO << "CellComplexData already pushed";
+        return;
+    }
+
+
+    ///////  FAKE OBJECT
+
+    vtkFloatArray* pcoords = vtkFloatArray::New();
+    pcoords->SetNumberOfComponents(3);
+    pcoords->SetNumberOfTuples(4);
+
+    float pts[4][3] = { {0.0, 0.0, 0.0}, {0.0, 1.0, 0.0},
+                        {1.0, 0.0, 0.0}, {1.0, 1.0, 0.0} };
+    for (int i=0; i<4; i++) {
+        pcoords->SetTuple(i, pts[i]);
+    }
+
+    vtkPoints* points = vtkPoints::New();
+    points->SetData(pcoords);
+
+    vtkCellArray* strips = vtkCellArray::New();
+    strips->InsertNextCell(4);
+    strips->InsertCellPoint(0);
+    strips->InsertCellPoint(1);
+    strips->InsertCellPoint(2);
+    strips->InsertCellPoint(3);
+
+    vtkIntArray* temperature = vtkIntArray::New();
+    temperature->SetName("Temperature");
+    temperature->InsertNextValue(10);
+    temperature->InsertNextValue(20);
+    temperature->InsertNextValue(30);
+    temperature->InsertNextValue(40);
+
+    vtkDoubleArray* vorticity = vtkDoubleArray::New();
+    vorticity->SetName("Vorticity");
+    vorticity->InsertNextValue(2.7);
+    vorticity->InsertNextValue(4.1);
+    vorticity->InsertNextValue(5.3);
+    vorticity->InsertNextValue(3.4);
+
+    vtkPolyData* polydata = vtkPolyData::New();
+    polydata->SetPoints(points);
+    polydata->SetStrips(strips);
+    polydata->GetPointData()->SetScalars(temperature);
+    polydata->GetPointData()->AddArray(vorticity);
+
+    ///// END OF FAKE
+
+    vtkPolyDataMapper* mapper = vtkPolyDataMapper::New();
+    mapper->SetInputData(polydata);
+    mapper->SetScalarRange(0, 40);
+
+    vtkActor* actor = vtkActor::New();
+    actor->SetMapper(mapper);
+
+    vtkRenderWindowInteractor *interactor = vtkRenderWindowInteractor::New();
+    interactor->SetRenderWindow(d->window);
+
+    d->renderer->AddActor(actor);
+
+    d->window->Render();
+    interactor->Start();
+
+    dtkWarn() << Q_FUNC_INFO << "Object added";
+    return;
 }
 
 //
