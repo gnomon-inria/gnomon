52fe8694f19025afa9b3ab78f30c7fdb796b9863
Julien Wintz <julien.wintz@inria.fr>
2017-11-08 18:31:28 +0100

    Remove useless prints.

--- src/gnomonVisualization/gnomonView.cpp
+++ src/gnomonVisualization/gnomonView.cpp
@@ -222,8 +222,6 @@ void gnomonView::onInserted(vtkImageData *image)
     actor->setInteractor(d->widget->GetInteractor());
     actor->setVolume(image);
 
-    image->PrintSelf(std::cout, vtkIndent());
-
     d->renderer->AddActor(actor);
 }
 
