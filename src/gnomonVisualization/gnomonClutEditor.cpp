c80b2537ee9c63bde74c492dd653bd619240d02f
Julien Wintz <julien.wintz@inria.fr>
2017-11-13 16:37:12 +0100

    Fixing build tree.

--- src/gnomonVisualization/gnomonClutEditor.cpp
+++ src/gnomonVisualization/gnomonClutEditor.cpp
@@ -856,7 +856,7 @@ void gnomonClutEditor::updateTable(void) {
         d->colorTransferFunction->GetNodeValue(i, &vtk_vertex[0]);
         d->colorTransferFunction->GetColor(vtk_vertex[0], &vtk_color[0]);
         alpha = d->opacityTransferFunction->GetValue(vtk_vertex[0]);
-        gnomonClutEditorVertex *new_vertex = new gnomonClutEditorVertex(gnomonClutEditorMap(QPointF(vtk_vertex[0], alpha / 100), d->min, d->max, this->width(), 100, log), QColor(vtk_color[0] *255, vtk_color[1] * 255, vtk_color[2] * 255));
+        gnomonClutEditorVertex *new_vertex = new gnomonClutEditorVertex(gnomonClutEditorMap(QPointF(vtk_vertex[0], alpha / 100), d->min, d->max, this->width(), 100, d->button_log->isChecked()), QColor(vtk_color[0] *255, vtk_color[1] * 255, vtk_color[2] * 255));
         d->table->addVertex(new_vertex);
         connect(new_vertex, SIGNAL(moved(const QPointF&)), d->scene, SIGNAL(moved(const QPointF&)));
     }
