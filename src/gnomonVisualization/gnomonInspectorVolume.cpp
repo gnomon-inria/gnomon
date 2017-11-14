94e8786ca6f4bb8acd2cc872bd8797ee5ec2aae9
Côme Le Breton <come.le-breton@inria.fr>
2017-11-14 09:42:18 +0100

    Puts the editor in a pop up window

--- src/gnomonVisualization/gnomonInspectorVolume.cpp
+++ src/gnomonVisualization/gnomonInspectorVolume.cpp
@@ -23,6 +23,11 @@ class gnomonInspectorVolumePrivate
 {
 public:
     gnomonClutEditor *editor;
+
+public:
+    QPushButton *editor_bt;
+    QComboBox *clut_cb;
+
 };
 
 // /////////////////////////////////////////////////////////////////
@@ -31,14 +36,24 @@ public:
 
 gnomonInspectorVolume::gnomonInspectorVolume(QWidget *parent) : gnomonInspector(parent), d(new gnomonInspectorVolumePrivate)
 {
-    d->editor = new gnomonClutEditor(this);
+    d->editor = new gnomonClutEditor();
 
     QVBoxLayout *layout = new QVBoxLayout;
+    d->editor_bt = new QPushButton(this);
+    d->editor_bt->setText("Editor");
+    d->clut_cb = new QComboBox(this);
+    d->clut_cb->addItem("Clut 1");
+    d->clut_cb->addItem("Clut 2");
+    d->clut_cb->addItem("Clut 3");
     layout->setContentsMargins(0, 0, 0, 0);
     layout->setSpacing(0);
-    layout->addWidget(d->editor);
-
+    QHBoxLayout * hlayout = new QHBoxLayout;
+    hlayout->addWidget(d->clut_cb);
+    hlayout->addWidget(d->editor_bt);
+    layout->addLayout(hlayout);
     this->setLayout(layout);
+
+    connect(d->editor_bt, &QPushButton::clicked, [=]() {d->editor->show();});
 }
 
 // ///////////////////////////////////////////////////////////////////
