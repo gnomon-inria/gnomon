9291b779571b2daad7c3de86a2b6abd84acb8807
Olli <olivier.ali@inria.fr>
2018-07-19 10:38:18 +0200

    Creation gnomonAbstractDynamicForm and gnomonTime

---
+++ src/gnomonCore/gnomonAbstractDynamicForm.h
@@ -0,0 +1,59 @@
+// Version: $Id$
+//
+//
+
+// Commentary:
+//
+//
+
+// Change Log:
+//
+//
+
+// Code:
+
+#pragma once
+
+#include <gnomonCoreExport.h>
+
+#include <QtCore>
+
+#include <dtkCore>
+
+// ///////////////////////////////////////////////////////////////////
+//
+// ///////////////////////////////////////////////////////////////////
+
+class GNOMONCORE_EXPORT gnomonAbstractDynamicForm
+{
+public:
+             gnomonAbstractDynamicForm(void) = default;
+    virtual ~gnomonAbstractDynamicForm(void) {};
+
+    virtual gnomonAbstractDynamicForm* clone(void) const = 0;
+
+public:
+    virtual void setInitialTime(gnomonTime T_i) = 0;
+    virtual gnomonAbstractStaticForm* atTime(gnomonTime t) = 0;
+    
+};
+
+// ///////////////////////////////////////////////////////////////////
+// Give the concept the plugin machinery
+// ///////////////////////////////////////////////////////////////////
+
+DTK_DECLARE_OBJECT        (gnomonAbstractDynamicForm *)
+DTK_DECLARE_PLUGIN        (gnomonAbstractDynamicForm, GNOMONCORE_EXPORT)
+DTK_DECLARE_PLUGIN_FACTORY(gnomonAbstractDynamicForm, GNOMONCORE_EXPORT)
+DTK_DECLARE_PLUGIN_MANAGER(gnomonAbstractDynamicForm, GNOMONCORE_EXPORT)
+
+// /////////////////////////////////////////////////////////////////
+// Register to gnomonCore layer
+// /////////////////////////////////////////////////////////////////
+
+namespace gnomonCore {
+    DTK_DECLARE_CONCEPT(gnomonAbstractDynamicForm, GNOMONCORE_EXPORT, dynamicForm);
+}
+
+//
+// gnomonAbstractDynamicForm.h ends here
