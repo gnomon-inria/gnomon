caf8a2f0018898a80628020f973655d65211faed
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2018-07-31 11:04:44 +0200

    added parameters in models for scenario test

--- test_python/test_gnomonCore/test_gnomonModelSystemScenario.py
+++ test_python/test_gnomonCore/test_gnomonModelSystemScenario.py
@@ -16,7 +16,7 @@ import unittest
 
 import numpy as np
 
-from gnomoncore import gnomonTime
+from gnomoncore import gnomonTime, gnomonDiscreteDynamicForm
 from gnomoncore import gnomonAbstractSystemScenario, gnomonSystem
 from gnomoncore import gnomonAbstractModel
 from gnomoncore import gnomonAbstractForm, gnomonSphereForm
@@ -30,27 +30,40 @@ class gnomonWallForm(gnomonAbstractForm):
         return "WallForm"
 
 
-class gnomonWallMotionModel(gnomonAbstractModel):
+class myWallEvolutionModel(gnomonAbstractModel):
     def __init__(self):
         self.wall = None
-        self.wall_x_0 = 2.
-        self.wall_x_1 = 4.
-        self.time_1 = 10
+        self.parameters = {'wall_x_0':2., 'wall_x_1':4., 'time_1':10}
 
     def setWall(self, wall):
         self.wall = wall
 
     def step(self, time, dt):
-        if(time < self.time_1):
-            self.wall.x = self.wall_x_0
+        if(time < self.parameters['time_1']):
+            self.wall.x = self.parameters['wall_x_0']
         else:
-            self.wall.x = self.wall_x_1
+            self.wall.x = self.parameters['wall_x_1']
 
-class gnomonSphereExpansionModel(gnomonAbstractModel):
+
+class mySphereEvolutionModel(gnomonAbstractModel):
     def __init__(self):
         self.sphere = None
-        self.growth_rate = 1.
-        self.growth_rate_decay = 0.99
+        self.parameters = {'growth_rate':1., 'growth_rate_decay':0.99}
+
+    def setSphere(self, sphere):
+        self.sphere = sphere
+
+    def step(self, time, dt):
+        self.parameters['growth_rate'] *= self.parameters['growth_rate_decay']
+        new_radius = self.sphere.radius() + self.parameters['growth_rate']*dt
+        self.sphere.setRadius(new_radius)
+
+
+class mySphereWallInteractionEvolutionModel(gnomonAbstractModel):
+    def __init__(self):
+        self.sphere = None
+        self.wall = None
+        self.parameters = {}
 
     def setSphere(self, sphere):
         self.sphere = sphere
@@ -59,26 +72,6 @@ class gnomonSphereExpansionModel(gnomonAbstractModel):
         self.wall = wall
 
     def step(self, time, dt):
-        self.growth_rate *= self.growth_rate_decay
-        self.sphere.setRadius(self.sphere.radius() + self.growth_rate*dt)
-
-
-class sphereExpansionAgainstWallScenario(gnomonAbstractSystemScenario):
-    def __init__(self, sphere=None, wall=None):
-        super(sphereExpansionAgainstWallScenario, self).__init__()
-        self.sphere = sphere
-        self.wall = wall
-
-        self.wall_model = gnomonWallMotionModel()
-        self.wall_model.setWall(self.wall)
-
-        self.sphere_model = gnomonSphereExpansionModel()
-        self.sphere_model.setSphere(self.sphere)
-
-    def step(self, time, dt):
-        self.wall_model.step(time,dt)
-        self.sphere_model.step(time,dt)
-
         if(abs(self.sphere.center()[0] - self.wall.x) <= self.sphere.radius() ):
             new_center = self.sphere.center()
             if(self.sphere.center()[0] > self.wall.x):
@@ -88,6 +81,28 @@ class sphereExpansionAgainstWallScenario(gnomonAbstractSystemScenario):
             self.sphere.setCenter(new_center)
 
 
+class mySphereExpansionAgainstWallScenario(gnomonAbstractSystemScenario):
+    def __init__(self, sphere=None, wall=None):
+        super(mySphereExpansionAgainstWallScenario, self).__init__()
+        self.sphere = sphere
+        self.wall = wall
+
+        self.wall_model = myWallEvolutionModel()
+        self.wall_model.setWall(self.wall)
+
+        self.sphere_model = mySphereEvolutionModel()
+        self.sphere_model.setSphere(self.sphere)
+
+        self.interaction_model = mySphereWallInteractionEvolutionModel()
+        self.interaction_model.setSphere(self.sphere)
+        self.interaction_model.setWall(self.wall)
+
+    def step(self, time, dt):
+        self.wall_model.step(time,dt)
+        self.sphere_model.step(time,dt)
+        self.interaction_model.step(time,dt)
+
+
 class TestModelSystemScenario(unittest.TestCase):
 
     def setUp(self):
@@ -95,16 +110,28 @@ class TestModelSystemScenario(unittest.TestCase):
         self.sphere.setRadius(1)
 
         self.wall = gnomonWallForm()
-        self.system_scenario = sphereExpansionAgainstWallScenario(self.sphere, self.wall)
+
+        self.system_scenario = mySphereExpansionAgainstWallScenario(self.sphere, self.wall)
+
+        self.dynamic_sphere = gnomonDiscreteDynamicForm()
+        self.dynamic_sphere.insert(self.system_scenario.sphere,gnomonTime(0))
+
+        # self.dynamic_wall = gnomonDiscreteDynamicForm()
+        # self.dynamic_wall.insert(self.system_scenario.wall,gnomonTime(0))
+
 
     def tearDown(self):
-        pass
+        self.dynamic_sphere.this.disown()
 
     def test_gnomonSphereExpansion(self):
         system = gnomonSystem(self.system_scenario)
         dt = 1
         for t in xrange(0, 20):
             system.step(t, dt)
+            self.dynamic_sphere.insert(self.system_scenario.sphere,gnomonTime(t+dt))
+            # self.dynamic_wall.insert(self.system_scenario.wall,gnomonTime(t+dt))
+
+        print self.dynamic_sphere.availableTimes()
 
         eps = 1e-4
         assert abs(self.sphere.radius() - 19.0272) < eps
