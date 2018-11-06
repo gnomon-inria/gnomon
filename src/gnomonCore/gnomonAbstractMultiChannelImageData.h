c29747452f79e87f794944ae8da9dfa0c2a2928b
Nicolas Niclausse <Nicolas.Niclausse@inria.fr>
2018-11-06 17:34:52 +0100

    use QStringList instead of QList<QString>

--- src/gnomonCore/gnomonAbstractMultiChannelImageData.h
+++ src/gnomonCore/gnomonAbstractMultiChannelImageData.h
@@ -35,7 +35,7 @@ public:
     virtual gnomonAbstractMultiChannelImageData* clone(void) const = 0;
 
 public:
-    virtual QList<QString> channelNames(void) = 0;
+    virtual QStringList channelNames(void) = 0;
 
     virtual void addChannelImage(const QString& channelName, vtkImageData *image) = 0;
     virtual vtkImageData* channelImage(const QString& channelName) const = 0;
