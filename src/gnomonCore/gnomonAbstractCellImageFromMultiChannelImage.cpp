3c1e866704596aaea74595c94b3cec9f4f8c9d03
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2018-11-07 10:55:29 +0100

    added abstraction for segmentation

---
+++ src/gnomonCore/gnomonAbstractCellImageFromMultiChannelImage.cpp
@@ -0,0 +1,24 @@
+// Version: $Id$
+//
+//
+
+// Commentary:
+//
+//
+
+// Change Log:
+//
+//
+
+// Code:
+
+#include "gnomonCore.h"
+#include "gnomonAbstractCellImageFromMultiChannelImage.h"
+
+namespace gnomonCore {
+    DTK_DEFINE_CONCEPT(gnomonAbstractCellImageFromMultiChannelImage, cellimageFromMultichannelimage, gnomonCore);
+}
+
+
+//
+// gnomonAbstractCellImageFromMultiChannelImage.cpp ends here
