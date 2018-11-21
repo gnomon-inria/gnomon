5bd36aaf61641c98b87b157639c8411722ca43a9
Pierre <pfernique@gmail.com>
2018-11-21 16:49:23 +0100

    Fix Fusion tests

--- tst/gnomonCore/gnomonImagesFusionCommandTest.cpp
+++ tst/gnomonCore/gnomonImagesFusionCommandTest.cpp
@@ -42,22 +42,27 @@ void gnomonImagesFusionCommandTestCase::init(void)
 
 void gnomonImagesFusionCommandTestCase::redo(void)
 {
-    QString image_0_file_path = QFINDTESTDATA("../resources/rect_t0.inr");
+    QString image_0_file_path = QFINDTESTDATA("../resources/time_0_cut_resampled.inr");
     d->serie_reader_command->setPath(image_0_file_path);
     d->serie_reader_command->redo();
-
     dtkImage *image_0 = d->serie_reader_command->at(0);
-    qDebug() << "image_0" << image_0;
 
-    QString image_1_file_path = QFINDTESTDATA("../resources/rect_t1.inr");
+    QString image_1_file_path = QFINDTESTDATA("../resources/time_0_cut_rotated1_resampled.inr");
     d->serie_reader_command->setPath(image_1_file_path);
     d->serie_reader_command->redo();
-
     dtkImage *image_1 = d->serie_reader_command->at(0);
 
-    d->fusion_command->addImage(image_0);
-    d->fusion_command->addImage(image_0);
+    QString image_2_file_path = QFINDTESTDATA("../resources/time_0_cut_rotated2_resampled.inr");
+    d->serie_reader_command->setPath(image_2_file_path);
+    d->serie_reader_command->redo();
+    dtkImage *image_2 = d->serie_reader_command->at(0);
 
+
+    d->fusion_command->addImage(image_0);
+    d->fusion_command->addImage(image_1);
+    d->fusion_command->addImage(image_2);
+ 
+    d->fusion_command->setParameter("nb_iterations", 0);
     d->fusion_command->redo();
 }
 
