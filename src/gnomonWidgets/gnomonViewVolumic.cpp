4e3f4fbf5c6fff4f40a7433add26ecba68a393f8
Nicolas Niclausse <Nicolas.Niclausse@inria.fr>
2018-11-23 10:44:54 +0100

    avoid crash when appling lut without image

--- src/gnomonWidgets/gnomonViewVolumic.cpp
+++ src/gnomonWidgets/gnomonViewVolumic.cpp
@@ -1175,6 +1175,8 @@ void gnomonViewVolumic::onSliceChanged(int slice)
 
 void gnomonViewVolumic::applyLut(const QMap<double, QColor>& source)
 {
+    if (!d->image_interactor->image)
+        return;
 
     vtkSmartPointer<vtkColorTransferFunction> color_function = vtkSmartPointer<vtkColorTransferFunction>::New();
     //color_function->RemoveAllPoints();
