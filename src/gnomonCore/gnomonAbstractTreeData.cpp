66085988777de5c675c06ca96af6a694fb4d0fd2
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2019-01-22 13:03:10 +0100

    added basic tree abstraction

---
+++ src/gnomonCore/gnomonAbstractTreeData.cpp
@@ -0,0 +1,22 @@
+// Version: $Id$
+//
+//
+
+// Commentary:
+//
+//
+
+// Change Log:
+//
+//
+
+// Code:
+
+#include "gnomonCore.h"
+#include "gnomonAbstractTreeData.h"
+
+namespace gnomonCore {
+    DTK_DEFINE_CONCEPT(gnomonAbstractTreeData, treeData, gnomonCore);
+}
+
+// gnomonAbstractTreeData.cpp ends here
---
+++ src/gnomonCore/gnomonAbstractTreeData.h
@@ -0,0 +1,75 @@
+// Version: $Id$
+//
+//
+
+// Commentary:
+//
+//
+
+// Change Log:
+//
+//
+
+// Code:
+
+#pragma once
+
+class dtkImage;
+
+#include <QtCore>
+
+#include <dtkCore>
+
+#include <gnomonCoreExport.h>
+
+// ///////////////////////////////////////////////////////////////////
+//
+// ///////////////////////////////////////////////////////////////////
+
+class GNOMONCORE_EXPORT gnomonAbstractTreeData
+{
+public:
+             gnomonAbstractTreeData(void) = default;
+    virtual ~gnomonAbstractTreeData(void) {};
+
+    virtual gnomonAbstractTreeData* clone(void) const = 0;
+
+public:
+    virtual void setImage(dtkImage *image) = 0;
+    virtual dtkImage* image(void) const = 0;
+
+    virtual QList<long> cellIds(void) const = 0;
+    virtual long cellCount(void) const = 0;
+    virtual QList<long> adjacentCellIds(long cellId) const = 0;
+    virtual long adjacentCellCount(long cellId) const = 0;
+
+    virtual QStringList cellPropertyNames(void) const = 0;
+    virtual bool hasCellProperty(const QString& propertyName) const = 0;
+
+    virtual QMap<long, QVariant> cellProperty(const QString& propertyName) const = 0;
+
+    virtual void addCellProperty(const QString& propertyName) = 0;
+    virtual void updateCellProperty(const QString& propertyName, const QMap<long, QVariant>& values, bool eraseProperty = true) = 0;
+    virtual void removeCellProperty(const QString& propertyName) = 0;
+
+    virtual QMap<long, QVariant> computeCellProperty(const QString& propertyName) = 0;
+};
+
+// ///////////////////////////////////////////////////////////////////
+// Give the concept the plugin machinery
+// ///////////////////////////////////////////////////////////////////
+
+DTK_DECLARE_OBJECT        (gnomonAbstractTreeData *)
+DTK_DECLARE_PLUGIN        (gnomonAbstractTreeData, GNOMONCORE_EXPORT)
+DTK_DECLARE_PLUGIN_FACTORY(gnomonAbstractTreeData, GNOMONCORE_EXPORT)
+DTK_DECLARE_PLUGIN_MANAGER(gnomonAbstractTreeData, GNOMONCORE_EXPORT)
+
+// /////////////////////////////////////////////////////////////////
+// Register to gnomonCore layer
+// /////////////////////////////////////////////////////////////////
+
+namespace gnomonCore {
+    DTK_DECLARE_CONCEPT(gnomonAbstractTreeData, GNOMONCORE_EXPORT, treeData);
+}
+
+// gnomonAbstractTreeData.h ends here
