90e02487de08dd9adc5e6e882443e958efe9b609
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2019-01-23 09:37:38 +0100

    added basic lstring abstraction

---
+++ src/gnomonCore/gnomonAbstractLStringData.cpp
@@ -0,0 +1,22 @@
+// Version: $Id$
+//
+//
+
+// Commentary:
+//
+//
+
+// Change Log:
+//
+//
+
+// Code:
+
+#include "gnomonCore.h"
+#include "gnomonAbstractLStringData.h"
+
+namespace gnomonCore {
+    DTK_DEFINE_CONCEPT(gnomonAbstractLStringData, lStringData, gnomonCore);
+}
+
+// gnomonAbstractLStringData.cpp ends here
