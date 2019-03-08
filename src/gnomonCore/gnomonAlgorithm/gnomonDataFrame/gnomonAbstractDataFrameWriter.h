cfeb2b4ffe04b1fad9b826216c9c8c3b7abae814
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2019-03-08 11:40:25 +0100

    added abstraction for writing dataframes

---
+++ src/gnomonCore/gnomonAlgorithm/gnomonDataFrame/gnomonAbstractDataFrameWriter.h
@@ -0,0 +1,63 @@
+// Version: $Id$
+//
+//
+
+// Commentary:
+//
+//
+
+// Change Log:
+//
+//
+
+// Code:
+
+#pragma once
+
+#include <gnomonCoreExport>
+
+#include <dtkCore>
+
+#include "gnomonAlgorithm/gnomonAbstractAlgorithm.h"
+
+class gnomonDataFrame;
+
+// ///////////////////////////////////////////////////////////////////
+//
+// ///////////////////////////////////////////////////////////////////
+
+class GNOMONCORE_EXPORT gnomonAbstractDataFrameWriter : public gnomonAbstractAlgorithm
+{
+public:
+    virtual ~gnomonAbstractDataFrameWriter(void) = default;
+
+public:
+    virtual void setParameter(const QString& parameterName, const QVariant& parameterValue) override = 0;
+    virtual QMap<QString, gnomonCoreParameter *> parameters(void) const override = 0;
+    virtual void run(void) override = 0;
+
+public:
+    virtual void setDataFrame(gnomonDataFrame *DataFrame) = 0;
+    virtual void setPath(const QString& path) = 0;
+
+};
+
+// ///////////////////////////////////////////////////////////////////
+// Give the concept the plugin machinery
+// ///////////////////////////////////////////////////////////////////
+
+DTK_DECLARE_OBJECT        (gnomonAbstractDataFrameWriter *)
+DTK_DECLARE_PLUGIN        (gnomonAbstractDataFrameWriter, GNOMONCORE_EXPORT)
+DTK_DECLARE_PLUGIN_FACTORY(gnomonAbstractDataFrameWriter, GNOMONCORE_EXPORT)
+DTK_DECLARE_PLUGIN_MANAGER(gnomonAbstractDataFrameWriter, GNOMONCORE_EXPORT)
+
+// /////////////////////////////////////////////////////////////////
+// Register to gnomonCore layer
+// /////////////////////////////////////////////////////////////////
+
+namespace gnomonCore {
+    DTK_DECLARE_CONCEPT(gnomonAbstractDataFrameWriter, GNOMONCORE_EXPORT, DataFrameWriter);
+}
+
+//
+// gnomonAbstractImageWriter.h ends here
