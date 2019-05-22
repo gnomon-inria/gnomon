6c582205da939ef3021188f3c096954396082678
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2019-05-22 17:54:32 +0200

    added poit cloud from image command

---
+++ src/gnomonCore/gnomonCommand/gnomonPointCloud/gnomonPointCloudFromImageCommand.cpp
@@ -0,0 +1,71 @@
+#include "gnomonPointCloudFromImageCommand.h"
+
+#include <dtkScript>
+
+#include <dtkImagingCore>
+
+class gnomonPointCloudFromImageCommandPrivate
+{
+public:
+    gnomonImagesSerie* input = nullptr;
+};
+
+gnomonPointCloudFromImageCommand::gnomonPointCloudFromImageCommand(const QString& key) : d(new gnomonPointCloudFromImageCommandPrivate)
+{
+    QString command = "import gnomonPointCloudFromImage";
+
+    int stat;
+
+    dtkScriptInterpreterPython::instance()->interpret(command, &stat);
+
+    Q_ASSERT(stat == dtkScriptInterpreter::Status::Status_Ok);
+
+    this->action = gnomonCore::pointCloudFromImage::pluginFactory().create(key);
+
+    Q_ASSERT(this->action);
+}
+
+gnomonPointCloudFromImageCommand::~gnomonPointCloudFromImageCommand(void)
+{
+    delete d;
+}
+
+void gnomonPointCloudFromImageCommand::redo(void)
+{
+    Q_ASSERT(this->action);
+
+    this->action->run();
+}
+
+void gnomonPointCloudFromImageCommand::undo(void)
+{
+    ((gnomonAbstractPointCloudFromImage *) this->action)->setInput(nullptr);
+}
+
+void gnomonPointCloudFromImageCommand::setInput(gnomonImagesSerie *input)
+{
+    d->input = input;
+
+    Q_ASSERT(this->action);
+    ((gnomonAbstractPointCloudFromImage *) this->action)->setInput(d->input);
+}
+
+void gnomonPointCloudFromImageCommand::setParameter(const QString& parameter, const QVariant& value)
+{
+    this->action->setParameter(parameter, value);
+}
+
+QMap<QString, gnomonCoreParameter *> gnomonPointCloudFromImageCommand::parameters(void) const
+{
+    return this->action->parameters();
+}
+
+gnomonImagesSerie *gnomonPointCloudFromImageCommand::input(void)
+{
+    return ((gnomonAbstractPointCloudFromImage *) this->action)->input();
+}
+
+gnomonPointCloud *gnomonPointCloudFromImageCommand::output(void)
+{
+    return ((gnomonAbstractPointCloudFromImage *) this->action)->output();
+}
