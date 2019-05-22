15f63700896f832ed326d26d066df661c99afbdb
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2019-05-22 18:11:25 +0200

    added the detection workspace

---
+++ src/gnomonWorkspace/gnomonWorkspacePointCloudFromImage.h
@@ -0,0 +1,42 @@
+// Version: $Id$
+//
+//
+
+// Commentary:
+//
+//
+
+// Change Log:
+//
+//
+
+// Code:
+
+#pragma once
+
+#include <gnomonWorkspaceExport>
+
+#include "gnomonWorkspace.h"
+
+class GNOMONWORKSPACE_EXPORT gnomonWorkspacePointCloudFromImage : public gnomonWorkspace
+{
+    Q_OBJECT
+
+public:
+     gnomonWorkspacePointCloudFromImage(QWidget *parent = nullptr);
+    ~gnomonWorkspacePointCloudFromImage(void);
+
+public:
+    void enter(void) override {}
+    void leave(void) override {}
+
+public slots:
+    void apply(void) override;
+    void configure(const QString& text);
+
+private:
+    class gnomonWorkspacePointCloudFromImagePrivate *d;
+};
+
+//
+// gnomonWorkspacePointCloudFromImage.h ends here
