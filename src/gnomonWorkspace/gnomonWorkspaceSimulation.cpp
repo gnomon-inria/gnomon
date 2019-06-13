c5b11515e8eed5976938065f591e6b30731f959b
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2019-06-13 14:49:44 +0200

    restored the fem simulation workspace

--- src/gnomonWorkspace/gnomonWorkspaceSimulation.cpp
+++ src/gnomonWorkspace/gnomonWorkspaceSimulation.cpp
@@ -93,7 +93,7 @@ void gnomonWorkspaceSimulation::apply(void)
     qDebug()<<"Command OK";
     d->command->setMesh(d->source->mesh());
     d->command->redo();
-//    d->target->setMesh((gnomonMesh *)d->command->updatedMesh()->clone());
+    d->target->setMesh(d->command->updatedMesh());
 
 }
 
