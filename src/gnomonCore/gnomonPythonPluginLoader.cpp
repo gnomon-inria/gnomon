9cb55c1e670e706f90081b4abc4774f6297e4a04
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2019-09-25 16:42:17 +0200

    minor changes on python loader

--- src/gnomonCore/gnomonPythonPluginLoader.cpp
+++ src/gnomonCore/gnomonPythonPluginLoader.cpp
@@ -21,15 +21,11 @@
 void loadPluginGroup (const QString& module)
 {
   int stat;
-  QString code = "import importlib\n";
-  code = code + QString("from pkg_resources import iter_entry_points\n");
+  QString code = "from pkg_resources import iter_entry_points\n";
   code = code + QString("for entry_point in iter_entry_points(group=") + QString("\"") + module  + QString("\"") + QString(", name=None):\n");
-  code = code + QString("   importlib.import_module(entry_point.module_name)\n");
-  code = code + QString("   print(entry_point)\n");
+  code = code + QString("   entry_point.load()\n");
 
   dtkScriptInterpreterPython::instance()->interpret(code, &stat);
 
   Q_ASSERT(stat == dtkScriptInterpreter::Status::Status_Ok);
-
-  qDebug()<<Q_FUNC_INFO<<"PLUGIN"<<module;
 }
