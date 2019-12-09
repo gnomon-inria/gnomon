d8b9744d8a555316c3ef0f78dff33782dd9bdb9d
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2019-12-09 15:36:29 +0100

    added a folder for the conda recipe

---
+++ conda/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_PREFIX_PATH="${PREFIX}" -DCMAKE_INSTALL_LIBDIR=lib
+
+make -j${CPU_COUNT}
+make install
+if [ `uname` == Darwin ]
+then
+    cp $RECIPE_DIR/gnomon $PREFIX/bin
+fi
