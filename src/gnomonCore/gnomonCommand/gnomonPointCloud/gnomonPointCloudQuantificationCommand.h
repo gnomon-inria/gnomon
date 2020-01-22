2a30b07c3068866534cfc4550078ac789a870c93
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2020-01-22 10:12:16 +0100

    added command for point cloud analysis

---
+++ src/gnomonCore/gnomonCommand/gnomonPointCloud/gnomonPointCloudQuantificationCommand.h
@@ -0,0 +1,28 @@
+#include "gnomonCommand/gnomonAbstractCommand.h"
+
+class GNOMONCORE_EXPORT gnomonPointCloudQuantificationCommand : public gnomonAbstractCommand
+{
+public:
+     gnomonPointCloudQuantificationCommand(void) = delete;
+     gnomonPointCloudQuantificationCommand(const QString&);
+    ~gnomonPointCloudQuantificationCommand(void);
+
+public:
+    void redo(void) override;
+    void undo(void) override;
+
+public:
+    void setImage(gnomonImageSeries *image);
+    void setPointCloud(gnomonPointCloudSeries *pointCloud);
+
+    gnomonPointCloudSeries *pointCloud(void);
+    gnomonDataFrameSeries *dataFrame(void);
+
+    virtual void setParameter(const QString&, const QVariant&);
+
+public:
+    QMap<QString, gnomonCoreParameter *> parameters(void) const;
+
+private:
+    class gnomonPointCloudQuantificationCommandPrivate *d;
+};
