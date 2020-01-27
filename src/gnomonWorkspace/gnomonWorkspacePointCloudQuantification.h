4da60a70b9214d846c6e6ac1ea1428c271a3f78b
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2020-01-27 17:44:25 +0100

    added the nuclei analysis workspace

---
+++ src/gnomonWorkspace/gnomonWorkspacePointCloudQuantification.h
@@ -0,0 +1,45 @@
+// Version: $Id$
+//
+//
+
+// Commentary:
+//
+//
+
+// Change Log:
+//
+//
+
+// Code:
+
+#pragma once
+
+#include <gnomonWorkspaceExport>
+
+#include <dtkWidgets>
+
+class GNOMONWORKSPACE_EXPORT gnomonWorkspacePointCloudQuantification : public dtkWidgetsWorkspace
+{
+    Q_OBJECT
+
+public:
+     gnomonWorkspacePointCloudQuantification(QWidget *parent = nullptr);
+    ~gnomonWorkspacePointCloudQuantification(void);
+
+public:
+    void enter(void) override;
+    void leave(void) override;
+
+public slots:
+    void apply(void) override;
+    void configure(const QString& text);
+
+public:
+    static const QColor color;
+
+private:
+    class gnomonWorkspacePointCloudQuantificationPrivate *d;
+};
+
+//
+// gnomonWorkspacePointCloudQuantification.h ends here
