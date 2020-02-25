66dffd196b93dd6ca2ae0c8157ef6baa512081a5
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2020-02-25 17:23:13 +0100

    used empty test in workspace menu building

--- app/gnomon/main.cpp
+++ app/gnomon/main.cpp
@@ -156,10 +156,21 @@ int main(int argc, char **argv)
     application->setOrganizationDomain("fr");
     application->installEventFilter(new gnomonEventFilter());
 
+    QPixmap pixmap(":gnomon/gnomon_logo.png");
+    qDebug()<<pixmap;
+    QSplashScreen *splash = new QSplashScreen(pixmap);
+    splash->show();
+
+    splash->showMessage("Preparing parsers",Qt::AlignCenter|Qt::AlignBaseline);
+    application->processEvents();
+
     QCommandLineParser *parser = application->parser();
     parser->setApplicationDescription("gnomon application.");
 
     application->initialize();
+
+    splash->showMessage("Loading core functionalities",Qt::AlignCenter|Qt::AlignBaseline);
+    application->processEvents();
 
     QCommandLineOption verboseOption("verbose", QCoreApplication::translate("main", "verbose plugin initialization"));
 
@@ -179,14 +190,25 @@ int main(int argc, char **argv)
     gnomonVisualization::initialize();
     gnomonWidgets::initialize();
 
+    splash->showMessage("Initializing Python interpreter",Qt::AlignCenter|Qt::AlignBaseline);
+    application->processEvents();
+
     bool redirect_io = false; int stat;
 
     dtkScriptInterpreterPython::instance()->init("gnomon-core");
 
+    splash->showMessage("Loading reader plugins",Qt::AlignCenter|Qt::AlignBaseline);
+    application->processEvents();
+
     gnomonMainWindow *window = new gnomonMainWindow;
+
+    splash->showMessage("Assembling application window",Qt::AlignCenter|Qt::AlignBaseline);
+    application->processEvents();
+
     window->setWindowTitle("gnomon");
     window->show();
     window->raise();
+    splash->finish(window);
 
     int status = application->exec();
 
