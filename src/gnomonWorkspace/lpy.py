124bd804098d05f400e463c506fa66dd3a2b5c7d
Julien Wintz <julien.wintz@inria.fr>
2020-02-26 21:33:10 +0100

    Some styling.

--- src/gnomonWorkspace/lpy.py
+++ src/gnomonWorkspace/lpy.py
@@ -28,62 +28,50 @@ from openalea.lpy.gui.lpystudio import LPyWindow, LpyPlotter, Viewer
 from openalea.lpy.gui.lpycodeeditor import LpyCodeEditor
 from openalea.lpy.gui.lpyview3d import LpyView3D
 
+from dtkthemes import dtkThemesEngine
+
+base1 = QColor(dtkThemesEngine.instance().value("@base1"))
+
 Viewer.show()
-Viewer.frameGL.setBgColor(40, 42, 54) # Just a PoC !!! -> dtk-themes-python to go
-#Viewer.hide()
+Viewer.frameGL.setBgColor(base1.red(), base1.green(), base1.blue())
 
-
-
-# print(dir(Viewer))
-# print(dir(Viewer.frameGL))
 workspace = LPyWindow()
 
 axiomviewer = LpyView3D(workspace)
 axiomviewer.setObjectName('LPYAxiomViewer')
 axiomviewer.show()
 
-
-#viewer = LpyView3D(workspace)
-#viewer.setObjectName('LPYViewer')
-#viewer.show()
-#workspace.view3D = viewer
-#workspace.use_own_view3D = True
-
-
-class LpyPlotter:
-    def __init__(self):
-        pass
-    def plot(self,scene):
-        viewer.display(scene)
-    def selection(self):
-        pass
-    def waitSelection(self,txt):
-        pass
-    def save(self,fname,format):
-        viewer.saveToSnapshot(fname)
-
-#plotter = LpyPlotter()
-
-#from openalea.lpy import registerPlotter
-#registerPlotter(plotter)
-
-
 workspace.frame.setObjectName("LPYCodeEditor")
-workspace.shellwidget.setObjectName("LPYShell")
-workspace.debugDock.setObjectName("LPYDebug")
 
+workspace.codeeditor.setStyleSheet(
+    ""
+    "background-color: " + dtkThemesEngine.instance().value("@base1") + ";"
+    "color: " + dtkThemesEngine.instance().value("@fg") + ";")
+
+workspace.shellwidget.setObjectName("LPYShell")
+workspace.shellwidget.setStyleSheet(
+    ""
+    "background-color: " + dtkThemesEngine.instance().value("@base1") + ";"
+    "color: " + dtkThemesEngine.instance().value("@fg") + ";")
+
+workspace.shellwidget._display_banner = False
+workspace.shellwidget.style_sheet  = ""
+workspace.shellwidget.style_sheet += ".error { color: orange; }"
+workspace.shellwidget.style_sheet += ".in-prompt { color: lightgreen; }"
+workspace.shellwidget.style_sheet += ".in-prompt-number { color: lightgreen; font-weight: bold; }"
+workspace.shellwidget.style_sheet += ".out-prompt { color: orange; }"
+workspace.shellwidget.style_sheet += ".out-prompt-number { color: orange; font-weight: bold; }"
+
+workspace.debugDock.setObjectName("LPYDebug")
 # workspace.parameterDock.toggleViewAction().toggle()
 # workspace.parameterDock.setObjectName("LPYParameters")
-
 workspace.scalarDock.setObjectName("LPYScalars")
 workspace.materialDock.setObjectName("LPYMaterials")
-
-# workspace.viewer.setObjectName("PGLViewer")
-
 workspace.setObjectName("LPYMainWindow")
-
 workspace.show()
 workspace.hide()
+
+# dtkThemesEngine.instance().changed.connect(lambda: print("Oh yeah"))
 
 #
 # lpy.py ends here
