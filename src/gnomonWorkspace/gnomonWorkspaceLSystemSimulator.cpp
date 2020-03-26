558d04fe3c5dfca9c9434ef0361e252b948595e7
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2020-03-26 11:41:00 +0100

    disable lpy control buttons while anything is running to avoid crashes

--- src/gnomonWorkspace/gnomonWorkspaceLSystemSimulator.cpp
+++ src/gnomonWorkspace/gnomonWorkspaceLSystemSimulator.cpp
@@ -606,6 +606,12 @@ void gnomonWorkspaceLSystemSimulator::reparentAction(QMenuBar * menu, const char
 
                     this->connect(button, &QPushButton::clicked, [=] (void) -> void
                     {
+                        d->run_button->setEnabled(false);
+                        d->step_button->setEnabled(false);
+                        d->rewind_button->setEnabled(false);
+                        d->animate_button->setEnabled(false);
+                        d->stop_button->setEnabled(false);
+
                         int stat;
 
                         if (d->use_axiom->isChecked()) {
@@ -652,6 +658,12 @@ void gnomonWorkspaceLSystemSimulator::reparentAction(QMenuBar * menu, const char
                         add_statement += QString::number(d->target->figureNumber());
                         add_statement += ")";
                         dtkScriptInterpreterPython::instance()->interpret(add_statement, &stat);
+
+                        d->run_button->setEnabled(true);
+                        d->step_button->setEnabled(true);
+                        d->rewind_button->setEnabled(true);
+                        d->animate_button->setEnabled(true);
+                        d->stop_button->setEnabled(true);
                     });
                 }
             }
