1f92ed763dd5726a6fd67d6953a4b70b05033a88
Tristan Cabel <tristan.cabel@inria.fr>
2021-12-06 12:51:54 +0100

    remove unnecessary build option for conda

--- conda/build.sh
+++ conda/build.sh
@@ -14,7 +14,6 @@ cmake .. \
       -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
       -DCMAKE_PREFIX_PATH="${PREFIX}" \
       -DCMAKE_INSTALL_LIBDIR=lib
-      -DGNOMON_ENABLE_WIDGETS=OFF
 
 make -j${CPU_COUNT}
 make install
