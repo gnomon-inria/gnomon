e819bc9ce0d02a647f70faacf56934273a62ae3b
Arthur Luciani <arthur.luciani@inria.fr>
2021-12-10 11:20:18 +0100

    Added abstract algorithm for BinaryImage

---
+++ src/gnomonCore/gnomonAlgorithm/gnomonBinaryImage/gnomonAbstractBinaryImageReader.cpp
@@ -0,0 +1,24 @@
+// Version: $Id$
+//
+//
+
+// Commentary:
+//
+//
+
+// Change Log:
+//
+//
+
+// Code:
+
+#include "gnomonCore.h"
+#include "gnomonAbstractBinaryImageReader.h"
+
+namespace gnomonCore {
+    DTK_DEFINE_CONCEPT(gnomonAbstractBinaryImageReader, binaryImageReader, gnomonCore);
+}
+
+
+//
+// gnomonAbstractBinaryImageReader.cpp ends here
