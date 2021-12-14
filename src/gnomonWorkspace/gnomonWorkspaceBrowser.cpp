b5dc45f7fae39fc11ff7d5ca569084ee25b6dfe2
Arthur Luciani <arthur.luciani@inria.fr>
2021-12-14 11:23:35 +0100

    restored this->pipeline->addClonedForm(binaryImage_series, this->browse_view->binaryImage()); now that gnomonBinaryImageSeries *gnomonViewForm::binaryImage(void) exists

--- src/gnomonWorkspace/gnomonWorkspaceBrowser.cpp
+++ src/gnomonWorkspace/gnomonWorkspaceBrowser.cpp
@@ -499,8 +499,7 @@ void gnomonWorkspaceBrowserPrivate::readForm(const QString& reader_plugin)
             qWarning() << Q_FUNC_INFO << "Resulting binaryImage series is void.";
         } else {
             this->browse_view->setForm("gnomonBinaryImage",binaryImage_series->clone());
-            // TODO : restore when view is done
-            //this->pipeline->addClonedForm(binaryImage_series, this->browse_view->binaryImage());
+            this->pipeline->addClonedForm(binaryImage_series, this->browse_view->binaryImage());
 //            this->view_stack->setCurrentWidget(this->browse_view);
             this->pipeline->addReader(binaryImageCommand);
         }
