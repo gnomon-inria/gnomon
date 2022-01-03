ba8a8f18862af5ede96a3b01cb3ff0b7f95a6013
Arthur Luciani <arthur.luciani@inria.fr>
2022-01-03 14:43:30 +0100

    beautifying

--- src/gnomonCore/gnomonAlgorithm/gnomonBinaryImage/gnomonAbstractBinaryImageFromImage.h
+++ src/gnomonCore/gnomonAlgorithm/gnomonBinaryImage/gnomonAbstractBinaryImageFromImage.h
@@ -21,19 +21,19 @@ public:
 
     // Outputs
 public:
-    virtual gnomonImageSeries *input(void) = 0;
-    virtual gnomonBinaryImageSeries *initialization(void) = 0;
-    virtual gnomonBinaryImageSeries *output(void) const = 0;
+    virtual gnomonImageSeries *input() = 0;
+    virtual gnomonBinaryImageSeries *initialization() = 0;
+    virtual gnomonBinaryImageSeries *output() const = 0;
 
 
 public:
-  virtual void setParameter(const QString& parameterName, const QVariant& parameterValue) override = 0;
-  virtual dtkCoreParameters parameters(void) const override = 0;
+    void setParameter(const QString& parameterName, const QVariant& parameterValue) override = 0;
+    virtual dtkCoreParameters parameters() const override = 0;
 
 
 public:
-    virtual void run(void) override = 0;
-    virtual QString documentation(void) override = 0;
+    void run() override = 0;
+    virtual QString documentation() override = 0;
 
 };
 
