c1018cff42a0beb2f19f8a21665ccabc0c674941
CABEL Tristan <tristan.cabel@inria.fr>
2022-01-14 14:41:00 +0100

    get matrix transformation as dataDict from imageRegistration

---
+++ src/gnomonCore/gnomonAlgorithm/gnomonDataDict/gnomonAbstractDataDictReader.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <gnomonCoreExport.h> 
+#include <dtkCore>
+#include "gnomonAlgorithm/gnomonAbstractAlgorithm.h"
+#include "gnomonForm/gnomonDataDict/gnomonDataDict.h"
+
+// ///////////////////////////////////////////////////////////////////
+//
+// ///////////////////////////////////////////////////////////////////
+
+class GNOMONCORE_EXPORT gnomonAbstractDataDictReader : public gnomonAbstractAlgorithm
+{
+public:
+    virtual ~gnomonAbstractDataDictReader(void) = default;
+
+public:
+    virtual void setParameter(const QString& parameterName, const QVariant& parameterValue) override = 0;
+    virtual dtkCoreParameters parameters(void) const override = 0;
+    virtual void run(void) override = 0;
+    virtual QString documentation(void) override = 0;
+
+public:
+    virtual void setPath(const QString& path) = 0;
+
+public:
+    virtual gnomonDataDictSeries *DataDict(void) = 0;
+
+public:
+    virtual QStringList extensions(void) = 0;
+};
+
+// ///////////////////////////////////////////////////////////////////
+// Give the concept the plugin machinery
+// ///////////////////////////////////////////////////////////////////
+
+DTK_DECLARE_OBJECT(gnomonAbstractDataDictReader *)
+DTK_DECLARE_PLUGIN(gnomonAbstractDataDictReader, GNOMONCORE_EXPORT)
+DTK_DECLARE_PLUGIN_FACTORY(gnomonAbstractDataDictReader, GNOMONCORE_EXPORT)
+DTK_DECLARE_PLUGIN_MANAGER(gnomonAbstractDataDictReader, GNOMONCORE_EXPORT)
+
+// /////////////////////////////////////////////////////////////////
+// Register to gnomonCore layer
+// /////////////////////////////////////////////////////////////////
+
+namespace gnomonCore {
+    DTK_DECLARE_CONCEPT(gnomonAbstractDataDictReader, GNOMONCORE_EXPORT, DataDictReader);
+}
+
+//
+// gnomonAbstractDataDictReader.h ends here
---
+++ src/gnomonCore/gnomonAlgorithm/gnomonDataDict/gnomonAbstractDataDictWriter.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <gnomonCoreExport.h> 
+#include <dtkCore>
+#include "gnomonAlgorithm/gnomonAbstractAlgorithm.h"
+#include "gnomonForm/gnomonDataDict/gnomonDataDict.h"
+
+// ///////////////////////////////////////////////////////////////////
+//
+// ///////////////////////////////////////////////////////////////////
+
+class GNOMONCORE_EXPORT gnomonAbstractDataDictWriter : public gnomonAbstractAlgorithm
+{
+public:
+    virtual ~gnomonAbstractDataDictWriter(void) = default;
+
+public:
+    virtual void setParameter(const QString& parameterName, const QVariant& parameterValue) override = 0;
+    virtual dtkCoreParameters parameters(void) const override = 0;
+    virtual void run(void) override = 0;
+    virtual QString documentation(void) override = 0;
+
+public:
+    virtual void setDataDict(gnomonDataDictSeries *datadict) = 0;
+    virtual void setPath(const QString& path) = 0;
+
+public:
+    virtual QStringList extensions(void) = 0;
+};
+
+// ///////////////////////////////////////////////////////////////////
+// Give the concept the plugin machinery
+// ///////////////////////////////////////////////////////////////////
+
+DTK_DECLARE_OBJECT(gnomonAbstractDataDictWriter *)
+DTK_DECLARE_PLUGIN(gnomonAbstractDataDictWriter, GNOMONCORE_EXPORT)
+DTK_DECLARE_PLUGIN_FACTORY(gnomonAbstractDataDictWriter, GNOMONCORE_EXPORT)
+DTK_DECLARE_PLUGIN_MANAGER(gnomonAbstractDataDictWriter, GNOMONCORE_EXPORT)
+
+// /////////////////////////////////////////////////////////////////
+// Register to gnomonCore layer
+// /////////////////////////////////////////////////////////////////
+
+namespace gnomonCore {
+    DTK_DECLARE_CONCEPT(gnomonAbstractDataDictWriter, GNOMONCORE_EXPORT, DataDictWriter);
+}
+
+//
+// gnomonAbstractDataDictWriter.h ends here
