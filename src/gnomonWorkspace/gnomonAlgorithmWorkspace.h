d25e9f62f30e32ecf85361df0ceb73ecb00c51b2
Arthur Luciani <arthur.luciani@inria.fr>
2022-01-19 12:19:37 +0100

    typo (unwanted 'w' removed)

--- src/gnomonWorkspace/gnomonAlgorithmWorkspace.h
+++ src/gnomonWorkspace/gnomonAlgorithmWorkspace.h
@@ -26,7 +26,7 @@ public:
     Q_PROPERTY(gnomonViewFormList* targets READ targets CONSTANT);
     Q_PROPERTY(gnomonViewForm* source READ source CONSTANT); //for ease of use
     Q_PROPERTY(gnomonViewForm* target READ target CONSTANT); //for ease of use
-w
+
     Q_PROPERTY(QJSValue parameters READ parameters NOTIFY parametersChanged)
 
 signals:
