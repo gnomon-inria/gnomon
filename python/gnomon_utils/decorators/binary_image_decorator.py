97c0fc4f7ef3c2901b0f25cdc2faecbb1af6b40d
Arthur Luciani <arthur.luciani@inria.fr>
2022-02-07 10:01:32 +0100

    revert changes to gnomonBinaryImage default data_setter

--- python/gnomon_utils/decorators/binary_image_decorator.py
+++ python/gnomon_utils/decorators/binary_image_decorator.py
@@ -8,7 +8,7 @@ from .form_series import buildFormSeries, formDictFromSeries, is_form_series_mod
 load_plugin_group("binaryImageData")
 
 default_plugin = "binaryImageDataSpatialImage"
-default_setter = "setImage"
+default_setter = "set_image"
 default_attr = "_image"
 
 form_class = gnomonBinaryImage
