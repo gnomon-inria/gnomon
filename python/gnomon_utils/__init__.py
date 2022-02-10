62dc3b104016726fabb06d2a2e817c74284f86cc
Arthur Luciani <arthur.luciani@inria.fr>
2022-02-10 13:09:56 +0100

    adding formDataPlugin to root of package

--- python/gnomon_utils/__init__.py
+++ python/gnomon_utils/__init__.py
@@ -1,2 +1,2 @@
 from .gnomonDataDriverMongo import gnomonDataDriverMongo
-from .gnomonPlugin import load_plugin_group, corePlugin, visualizationPlugin, available_plugins, serialize
+from .gnomonPlugin import load_plugin_group, corePlugin, visualizationPlugin, formDataPlugin, available_plugins, serialize
