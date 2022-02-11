c8c4e372692a652bb186ec87fda800d260a74719
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2022-02-11 12:02:32 +0100

    fix module in available plugins function

--- src/gnomonCore/gnomonPythonPluginLoader.cpp
+++ src/gnomonCore/gnomonPythonPluginLoader.cpp
@@ -40,7 +40,7 @@ QStringList availablePluginsFromGroup(const QString & module) {
 
     Py_Initialize(); // NO OP if already initialized
 
-    PyObject* pName = PyUnicode_FromString("gnomon");
+    PyObject* pName = PyUnicode_FromString("gnomon.utils");
     PyObject* pModule = PyImport_Import(pName);
 
     if(pModule)
