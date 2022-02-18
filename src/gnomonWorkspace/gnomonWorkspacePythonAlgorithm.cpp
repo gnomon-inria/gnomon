a367e14267b4173141159eddebec130ed2f2722b
Arthur Luciani <arthur.luciani@inria.fr>
2022-02-18 10:31:14 +0100

    use default arg

--- src/gnomonWorkspace/gnomonWorkspacePythonAlgorithm.cpp
+++ src/gnomonWorkspace/gnomonWorkspacePythonAlgorithm.cpp
@@ -117,8 +117,8 @@ gnomonWorkspacePythonAlgorithm::gnomonWorkspacePythonAlgorithm(QObject *parent) 
 
     });
 
-    d->sources->addView(QVector<struct QString>());
-    d->targets->addView(QVector<struct QString>());
+    d->sources->addView();
+    d->targets->addView();
 
     d->pool->addView(this->source());
     d->pool->addView(this->target());
