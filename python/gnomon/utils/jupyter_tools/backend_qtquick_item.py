eda16c4006b764b99ebf17ace6e35b0e977765bb
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2022-02-23 14:40:10 +0100

    paint directly + remove zoom factor + route key events correctly

--- python/gnomon/utils/jupyter_tools/backend_qtquick_item.py
+++ python/gnomon/utils/jupyter_tools/backend_qtquick_item.py
@@ -1,3 +1,5 @@
+import logging
+
 from qtpy import QtQml
 from qtpy.QtCore import Qt, QObject, Slot, Signal, QEvent, QTimer, QUrl, qDebug, QRect, QPoint, QCoreApplication
 from qtpy.QtQuick import QQuickItem, QQuickPaintedItem
@@ -55,9 +57,12 @@ class JupyterConsole(QQuickPaintedItem):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.editor = None
-        self.texture = None
-        self.zoom_factor = 0.8
+        self.widget = None
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update)
+        self.timer.setInterval(1000/60.)
+        self.timer.start()
 
         self.setAcceptedMouseButtons(Qt.AllButtons)
         self.setFlag(QQuickItem.ItemAcceptsDrops, True)
@@ -65,34 +70,34 @@ class JupyterConsole(QQuickPaintedItem):
         self.setFlag(QQuickItem.ItemIsFocusScope, True)
         self.setFlag(QQuickItem.ItemAcceptsInputMethod, True)
 
-        self.initInterpreter()
+        self.init_widget()
 
     @Slot(QEvent)
     def focusInEvent(self, event):
-        print("focusInEvent")
+        logging.debug("focusInEvent")
         self.forceActiveFocus()
 
     @Slot()
-    def initInterpreter(self):
-        print("initInterpreter")
-        self.updateEditorSize()
-        self.editor = InProcessJupyterWidget()
-        self.editor.setObjectName("InProcessJupyterWidget")
+    def init_widget(self):
+        logging.debug("init_widget")
+        self.update_widget_size()
+        self.widget = InProcessJupyterWidget()
+        self.widget.setObjectName("InProcessJupyterWidget")
 
         # Connect signals
-        self.widthChanged.connect(self.updateEditorSize)
-        self.heightChanged.connect(self.updateEditorSize)
+        self.widthChanged.connect(self.update_widget_size)
+        self.heightChanged.connect(self.update_widget_size)
 
     def mouseMoveEvent(self, event):
-        print("mouseMoveEvent")
+        logging.debug("mouseMoveEvent")
         self.routeMouseEvents(event)
 
     def mousePressEvent(self, event):
-        print("mousePressEvent")
+        logging.debug("mousePressEvent")
         self.routeMouseEvents(event)
 
     def mouseReleaseEvent(self, event):
-        print("mouseReleaseEvent")
+        logging.debug("mouseReleaseEvent")
         self.routeMouseEvents(event)
 
     def mouseDoubleClickEvent(self, event):
@@ -102,98 +107,82 @@ class JupyterConsole(QQuickPaintedItem):
     def dragEnterEvent(self, event):
         event.accept()
         return
-        self.editor._control.dragEnterEvent(event)
+        self.widget._control.dragEnterEvent(event)
 
     def dragLeaveEvent(self, event):
         event.accept()
         return
-        self.editor._control.dragLeaveEvent(event)
+        self.widget._control.dragLeaveEvent(event)
 
     def dragMoveEvent(self, event):
         event.accept()
         return
-        self.editor._control.dragMoveEvent(event)
+        self.widget._control.dragMoveEvent(event)
 
     def routeMouseEvents(self, event):
-        print("routeMouseEvents")
+        logging.debug("routeMouseEvents")
         self.forceActiveFocus()
-        if self.editor is not None:
-            newEvent = QMouseEvent(event.type(), event.localPos(), event.button(), event.buttons(), event.modifiers())
-            if newEvent.type() == QEvent.MouseMove:
-                self.editor._control.mouseMoveEvent(newEvent)
-            elif newEvent.type() == QEvent.MouseButtonPress:
-                self.editor._control.mousePressEvent(newEvent)
-            elif newEvent.type() == QEvent.MouseButtonRelease:
-                self.editor._control.mouseReleaseEvent(newEvent)
-            elif newEvent.type() == QEvent.MouseButtonDblClick:
-                self.editor._control.mouseDoubleClickEvent(newEvent)
-            event.accept()
+        if self.widget is not None:
+            if event.type() == QEvent.MouseMove:
+                self.widget._control.mouseMoveEvent(event)
+            elif event.type() == QEvent.MouseButtonPress:
+                self.widget._control.mousePressEvent(event)
+            elif event.type() == QEvent.MouseButtonRelease:
+                self.widget._control.mouseReleaseEvent(event)
+            elif event.type() == QEvent.MouseButtonDblClick:
+                self.widget._control.mouseDoubleClickEvent(event)
             # TODO: Consider adding a MouseArea to catch scroll events
+            event.accept()
             self.update()
 
     def routeKeyEvents(self, event):
-        print("routeKeyEvents")
-        if self.editor is not None:
-            newEvent = QKeyEvent(event.type(), event.key(), event.modifiers())
-            if event.type() == QKeyEvent.KeyPress:
-                if (len(event.text()) > 0):
-                    ascii_key = ord(event.text())
-                else:
-                    ascii_key = 0
-                keysym = ascii_to_key_sym(ascii_key)
-                if keysym is not None:
-                    curs = self.editor._control.textCursor()
-                    curs.insertText(keysym)
-                else:
-                    QCoreApplication.postEvent(self.editor._control, newEvent)
-            event.accept()
-            self.update()
+        logging.debug("routeKeyEvents")
+        if self.widget is not None:
+            intercepted = self.widget.eventFilter(self.widget._control, event)
+            if not intercepted:
+                if event.type() == QEvent.KeyPress:
+                    self.widget._control.keyPressEvent(event)
+                elif event.type() == QEvent.KeyRelease:
+                    self.widget._control.keyReleaseEvent(event)
+        event.accept()
+        self.update()
 
     def keyPressEvent(self, event):
-        print("keyPressEvent")
+        logging.debug("keyPressEvent")
         self.routeKeyEvents(event)
 
     def keyReleaseEvent(self, event):
-        print("keyReleaseEvent")
+        logging.debug("keyReleaseEvent")
         self.routeKeyEvents(event)
 
-    def updateEditorSize(self):
-        if self.editor is not None:
-            self.editor.setGeometry(0, 0, int(self.width()/self.zoom_factor), int(self.height()/self.zoom_factor))
+    def update_widget_size(self):
+        if self.widget is not None:
+            self.widget.setGeometry(0, 0, int(self.width()), int(self.height()))
 
     def onCustomReplot(self):
         self.update()
 
     def paint(self, painter):
-        print("paintEvent")
-        if (self.editor is not None):
-            print("paintEvent 2")
-            self.setRenderTarget(QQuickPaintedItem.FramebufferObject)
-            rect = QRect(0, 0, int(self.width()/self.zoom_factor), int(self.height()/self.zoom_factor))
-            picture = QPixmap(rect.size())
-            picture.fill(QColor(0, 0, 0, 255))
-            #picture = self.editor.grab(QRect(QPoint(0, 0), QSize(-1, -1)))
-            self.editor.render(picture, QPoint(), QRegion(rect))
-            picture_resized = picture.scaled(int(self.width()), int(self.height()),
-                                             aspectRatioMode=Qt.KeepAspectRatio,
-                                             mode=Qt.SmoothTransformation)
-            painter.drawPixmap(QPoint(0, 0), picture_resized)
+        logging.debug("paintEvent")
+        if (self.widget is not None):
+            rect = QRect(0, 0, int(self.width()), int(self.height()))
+            self.widget.render(painter, QPoint(), rect)
 
     def getScreenshot(self, map):
         painter = QPainter(map)
         offset = 0
-        block = self.editor._control.document().firstBlock()
+        block = self.widget._control.document().firstBlock()
         while (block.isValid()):
-            r = self.editor._control.blockBoundingRect(block) # QRectF
+            r = self.widget._control.blockBoundingRect(block) # QRectF
             layout = block.layout()
             if (not block.isVisible()):
                 offset = offset + r.height()
-                block = block.next();
+                block = block.next()
                 continue
             else:
-                layout.draw(painter, QPoint(0,offset))
-            offset = offset + r.height();
-            block = block.next();
+                layout.draw(painter, QPoint(0, offset))
+            offset = offset + r.height()
+            block = block.next()
 
 
 QtQml.qmlRegisterType(JupyterConsole, "gnomon.Jupyter", 1, 0, "JupyterConsole")
--- python/gnomon/utils/jupyter_tools/backend_qtquick_widget.py
+++ python/gnomon/utils/jupyter_tools/backend_qtquick_widget.py
@@ -1,8 +1,6 @@
 from qtpy import QtCore
 from qtpy.QtCore import Qt
 
-from IPython.lib import guisupport
-
 from qtconsole.rich_jupyter_widget import RichJupyterWidget
 from qtconsole.inprocess import QtInProcessKernelManager
 
@@ -14,15 +12,13 @@ class InProcessJupyterWidget(RichJupyterWidget):
 
         self.kernel_manager = QtInProcessKernelManager()
         self.kernel_manager.start_kernel()
-        # kernel_manager.kernel.gui = 'qt4' # Ignored
         self.kernel_client = self._kernel_manager.client()
         self.kernel_client.start_channels()
 
         def stop():
             self.kernel_client.stop_channels()
             self.kernel_manager.shutdown_kernel()
-            # guisupport.get_app_qt4().exit()
 
         self.exit_requested.connect(stop)
-        self.setAttribute(Qt.WA_StyledBackground, True)
+
 
