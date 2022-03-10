54686e81e3c8c31a6d956d409fa52cda0075dde6
Arthur Luciani <arthur.luciani@inria.fr>
2022-03-10 18:48:14 +0100

    fixed bad group name for binaryImageFromImage

--- src/gnomonCore/gnomonCommand/gnomonBinaryImage/gnomonBinaryImageFromImageCommand.h
+++ src/gnomonCore/gnomonCommand/gnomonBinaryImage/gnomonBinaryImageFromImageCommand.h
@@ -34,7 +34,7 @@ public:
 
 public:
     static bool isEmpty();
-    inline static const QString groupName = "binaryImageReader";
+    inline static const QString groupName = "binaryImageFromImage";
     static QStringList availablePlugins();
 
 private:
