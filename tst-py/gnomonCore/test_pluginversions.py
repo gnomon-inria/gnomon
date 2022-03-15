0c3192568726739aa3c259a15095fc2b7c1a3c4b
CABEL Tristan <tristan.cabel@inria.fr>
2022-03-15 08:43:39 +0100

    0.60.0

--- tst-py/gnomonCore/test_pluginversions.py
+++ tst-py/gnomonCore/test_pluginversions.py
@@ -24,7 +24,7 @@ class dummyPlugin(gnomonAbstractDataDriver):
     def find(self, query):
         return [""]
 
-version_exact = "0.20.0"
+version_exact = "0.60.0"
 
 split = version_exact.split('.')
 split[1] = str(int(split[1]) + 1)
