cde63e32b6a0ee6e4358505938b0cf831b8d5e56
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2022-03-28 10:51:16 +0200

    update view times on form remove / clear

--- src/gnomonVisualization/gnomonView/gnomonViewForm.cpp
+++ src/gnomonVisualization/gnomonView/gnomonViewForm.cpp
@@ -1991,6 +1991,11 @@ void gnomonViewForm::removeForm(const QString& name)
     d->formVisibility.remove(name);
     d->forms.remove(name);
 
+    d->updateFormsTimes();
+    if (d->forms.isEmpty()) {
+        d->empty = true;
+    }
+
     this->render();
 
     emit formsChanged();
@@ -2213,6 +2218,8 @@ void gnomonViewForm::update(void)
 void gnomonViewForm::clear(void)
 {
     d->clear();
+    d->updateFormsTimes();
+    d->empty = true;
     emit formsChanged();
 }
 
