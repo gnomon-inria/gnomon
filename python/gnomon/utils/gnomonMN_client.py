27fc86f556f175aa7722e758632226759e51c795
Karamoko Samassa <karamoko.samassa@inria.fr>
2022-04-26 09:34:36 +0200

    first attempt to create a direct message system gnomon-morphoplot

---
+++ python/gnomon/utils/gnomonMN_client.py
@@ -0,0 +1,17 @@
+import socket
+import zmq
+import pickle
+
+'''
+Find server port,
+Connect client to server
+Some Specific parameters
+'''
+def handle_data():
+    # uncode data to be sent
+    # decode data received
+
+# disconnect from server til a connection signal received
+    
+
+    
---
+++ python/gnomon/utils/morphoplot_server.py
@@ -0,0 +1,26 @@
+import socket
+import zmq
+import pickle
+
+'''
+Config server port,
+Some Specific parameters
+'''
+
+def handle_client():
+    # set up client config
+    # decode data received from client
+    # encode data to be sent to client
+    pass
+
+def start():
+    # launch morphoplot into a local Port
+    # listen to client
+
+    pass
+
+if __name__ == "__main__":
+    start()
+
+
+    
