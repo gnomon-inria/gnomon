c2f2e0b55a22ba365d62873498e995aaa9583f3c
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2022-07-05 16:51:37 +0200

    set qml ownership of q_invokable returned pointers to cpp

--- src/gnomonPipeline/gnomonPipelineNode.cpp
+++ src/gnomonPipeline/gnomonPipelineNode.cpp
@@ -22,6 +22,8 @@
 
 #include <dtkCoreParameter>
 
+#include <QtQml>
+
 // /////////////////////////////////////////////////////////////////
 // gnomonPipelineNodePrivate
 // /////////////////////////////////////////////////////////////////
@@ -357,7 +359,9 @@ int gnomonPipelineNode::outputEdgeCount(void)
 gnomonPipelineEdge *gnomonPipelineNode::inputEdgeAt(int index)
 {
     if (index < d->input_edges.size()) {
-        return d->input_edges[index];
+        gnomonPipelineEdge* edge = d->input_edges[index];
+        QQmlEngine::setObjectOwnership(edge, QQmlEngine::CppOwnership);
+        return edge;
     } else {
         return nullptr;
     }
@@ -366,7 +370,9 @@ gnomonPipelineEdge *gnomonPipelineNode::inputEdgeAt(int index)
 gnomonPipelineEdge *gnomonPipelineNode::outputEdgeAt(int index)
 {
     if (index < d->output_edges.size()) {
-        return d->output_edges[index];
+        gnomonPipelineEdge* edge = d->output_edges[index];
+        QQmlEngine::setObjectOwnership(edge, QQmlEngine::CppOwnership);
+        return edge;
     } else {
         return nullptr;
     }
@@ -418,7 +424,9 @@ QStringList gnomonPipelineNode::outputPortsNames(void)
 gnomonPipelinePort *gnomonPipelineNode::inputPort(const QString& name)
 {
     if (d->input_ports.contains(name)) {
-        return d->input_ports[name];
+        gnomonPipelinePort *port = d->input_ports[name];
+        QQmlEngine::setObjectOwnership(port, QQmlEngine::CppOwnership);
+        return port;
     } else {
         return nullptr;
     }
@@ -427,7 +435,9 @@ gnomonPipelinePort *gnomonPipelineNode::inputPort(const QString& name)
 gnomonPipelinePort *gnomonPipelineNode::outputPort(const QString& name)
 {
     if (d->output_ports.contains(name)) {
-        return d->output_ports[name];
+        gnomonPipelinePort *port = d->output_ports[name];
+        QQmlEngine::setObjectOwnership(port, QQmlEngine::CppOwnership);
+        return port;
     } else {
         return nullptr;
     }
