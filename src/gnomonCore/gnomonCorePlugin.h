7770ea6da0b672e74da825c7bbc8a371f04be36e
CABEL Tristan <tristan.cabel@inria.fr>
2023-04-14 16:28:59 +0200

    add cpp plugins for segmentation workspace as a test

--- src/gnomonCore/gnomonCorePlugin.h
+++ src/gnomonCore/gnomonCorePlugin.h
@@ -8,21 +8,31 @@
 // GNOMON_DECLARE_PLUGIN_FACTORY
 // ///////////////////////////////////////////////////////////////////
 
-#define GNOMON_DECLARE_PLUGIN_FACTORY(type, Export)                     \
-    class Export type##PluginFactory : public gnomonPluginFactory<type> \
-    {                                                                   \
-    public:                                                             \
-        static type##PluginFactory& instance()                          \
-        {                                                               \
-            static type##PluginFactory _instance;                       \
-            return _instance;                                           \
-        };                                                              \
+#define GNOMON_DECLARE_PLUGIN_FACTORY(type, Export)                              \
+    class Export type##PluginFactory : public gnomonPluginFactory<type>          \
+    {                                                                            \
+    public:                                                                      \
+        static type##PluginFactory& instance()                                   \
+        {                                                                        \
+            static type##PluginFactory _instance;                                \
+            return _instance;                                                    \
+        };                                                                       \
+    };                                                                           \
+    class Export type##PluginManager : public dtkCorePluginManager<type##Plugin> \
+    {                                                                            \
+    public:                                                                      \
+        static type##PluginManager& instance()                                   \
+        {                                                                        \
+            static type##PluginManager _instance;                                \
+            return _instance;                                                    \
+        };                                                                       \
     };
 
 #define GNOMON_DECLARE_CONCEPT(type, Export, Namespace)     \
     namespace Namespace                                     \
     {                                                       \
         Export type##PluginFactory& pluginFactory();        \
+        Export type##PluginManager& pluginManager();        \
     }
 
 #define GNOMON_DEFINE_CONCEPT(type, Namespace, LayerName)   \
@@ -31,5 +41,9 @@
         type##PluginFactory& pluginFactory()                \
         {                                                   \
             return type##PluginFactory::instance();         \
+        }                                                   \
+        type##PluginManager& pluginManager()                \
+        {                                                   \
+            return type##PluginManager::instance();         \
         }                                                   \
     }
