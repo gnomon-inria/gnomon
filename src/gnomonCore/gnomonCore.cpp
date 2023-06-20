e5f7a0a411bd0d60725cc202f5b9b2d19331471e
CABEL Tristan <tristan.cabel@inria.fr>
2023-06-20 15:12:34 +0000

    Update gnomonCore.cpp

--- src/gnomonCore/gnomonCore.cpp
+++ src/gnomonCore/gnomonCore.cpp
@@ -47,7 +47,8 @@ void initialize(const QString& path, bool from_python)
     for(const QString& v_path : pathslist) {
         manager().initialize(v_path);
     }
-
+    
+    gnomonCore::meshData::pluginFactory().record("gnomonMeshDataStdVector", gnomonMeshDataStdVectorCreator);
     gnomonCore::gui_thread = QObject().thread();
 }
 
