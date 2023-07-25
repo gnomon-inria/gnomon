433ded9625b5353bfce29b4b7bc168def9ec9d67
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2023-07-25 12:04:19 +0200

    add wall concept in cell image abstraction

--- src/gnomonCore/gnomonForm/gnomonCellImage/gnomonAbstractCellImageData.h
+++ src/gnomonCore/gnomonForm/gnomonCellImage/gnomonAbstractCellImageData.h
@@ -52,6 +52,15 @@ public:
     virtual void removeCellProperty(const QString& propertyName) = 0;
 
     virtual QMap<long, QVariant> computeCellProperty(const QString& propertyName) = 0;
+
+    virtual QStringList wallPropertyNames(void) const = 0;
+    virtual bool hasWallProperty(const QString& propertyName) const = 0;
+
+    virtual QMap<long, QVariant> wallProperty(const QString& propertyName) const = 0;
+
+    virtual void addWallProperty(const QString& propertyName) = 0;
+    virtual void updateWallProperty(const QString& propertyName, const QMap<long, QVariant>& values, bool eraseProperty = true) = 0;
+    virtual void removeWallProperty(const QString& propertyName) = 0;
 };
 
 // ///////////////////////////////////////////////////////////////////
--- src/gnomonCore/gnomonForm/gnomonCellImage/gnomonCellImage.h
+++ src/gnomonCore/gnomonForm/gnomonCellImage/gnomonCellImage.h
@@ -100,6 +100,16 @@ public:
     virtual void removeCellProperty(const QString& propertyName) { return m_data->removeCellProperty(propertyName); }
 
     virtual QMap<long, QVariant> computeCellProperty(const QString& propertyName) { return m_data->computeCellProperty(propertyName); };
+
+    virtual QStringList wallPropertyNames(void) const { return m_data->wallPropertyNames(); };
+    virtual bool hasWallProperty(const QString& propertyName) const { return m_data->hasWallProperty(propertyName); }
+
+    virtual QMap<long, QVariant> wallProperty(const QString& propertyName) const { return m_data->wallProperty(propertyName); }
+
+    virtual void addWallProperty(const QString& propertyName) { return m_data->addWallProperty(propertyName); }
+    virtual void updateWallProperty(const QString& propertyName, const QMap<long, QVariant>& values, bool eraseProperty = true) { return m_data->updateWallProperty(propertyName, values, eraseProperty); }
+    virtual void removeWallProperty(const QString& propertyName) { return m_data->removeWallProperty(propertyName); }
+
 };
 
 // ///////////////////////////////////////////////////////////////////
