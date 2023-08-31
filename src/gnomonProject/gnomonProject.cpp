4c58bb63db283ad4bb7532bfcff672866d5e4eb2
Arthur Luciani <arthur.luciani@inria.fr>
2023-08-31 11:14:32 +0200

    early project files

--- src/gnomonProject/gnomonProject.cpp
+++ src/gnomonProject/gnomonProject.cpp
@@ -1,28 +1,65 @@
 #include "gnomonProject.h"
 
+
+#define PROJECT_INFO_FOLDER ".gnomon"
+#define PROJECT_BACKUP_FOLDER ".backup_gnomon"
+
 // /////////////////////////////////////////////////////////////////
 // gnomonProjectPrivate
 // /////////////////////////////////////////////////////////////////
 
-class gnomonProjectPrivate 
-{
+class gnomonProjectPrivate {
+
+public:
+    explicit gnomonProjectPrivate(const QString &path);
+    ~gnomonProjectPrivate();
+
 public:
 
+    QDir projectDir;
+    QDir currentDir;
+
 };
+
+gnomonProjectPrivate::gnomonProjectPrivate(const QString &path): projectDir(path), currentDir(path) {
+    QDir::setCurrent(path);
+}
 
 
 // /////////////////////////////////////////////////////////////////
 // gnomonProject
 // /////////////////////////////////////////////////////////////////
 
-gnomonProject::gnomonProject(QObject *parent) : QObject(parent)
-{
-    d = new gnomonProjectPrivate;
+
+
+gnomonProject::gnomonProject(const QString &path): QObject(nullptr) {
+    d = new gnomonProjectPrivate(path);
+
+    if(!d->projectDir.exists()) {
+        QDir::current().mkpath(path);
+    }
+
+    bool isProject = isDirAProject(d->projectDir);
+    if(isProject) {
+        readProjectInfo();
+    } else {
+        populateNewProject();
+    }
 }
 
 gnomonProject::~gnomonProject(void)
 {
     delete d;
+}
+
+void gnomonProject::populateNewProject() {
+    d->projectDir.mkdir(PROJECT_INFO_FOLDER);
+
+    d->projectDir.mkdir(PROJECT_BACKUP_FOLDER);
+}
+
+bool gnomonProject::isDirAProject(const QDir &dir) {
+    return dir.exists(PROJECT_INFO_FOLDER) && dir.exists(PROJECT_BACKUP_FOLDER);
 }
 
 //
--- src/gnomonProject/gnomonProject.h
+++ src/gnomonProject/gnomonProject.h
@@ -9,8 +9,19 @@ class GNOMONPROJECT_EXPORT gnomonProject : public QObject
     Q_OBJECT
 
 public:
-    gnomonProject(QObject *parent = nullptr);
-    ~gnomonProject(void);
+    gnomonProject(QObject *parent = nullptr) = delete;
+    explicit gnomonProject(const QString &path);
+    ~gnomonProject(void) override;
+
+public:
+    static gnomonProject *newProject(const QString &path, const QString &name);
+
+
+private:
+    bool isDirAProject(const QDir &dir);
+    void populateNewProject();
+    void readProjectInfo();
+
 private:
    class gnomonProjectPrivate *d;
 };
