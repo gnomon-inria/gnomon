3075f211579a9c72de9f08b7eeaa32112434f8e2
karamoko <karamoko.samassa@inria.fr>
2023-12-04 10:56:43 +0100

    overwriting existing project

--- src/gnomonProject/gnomonProjectManager.cpp
+++ src/gnomonProject/gnomonProjectManager.cpp
@@ -63,12 +63,22 @@ gnomonProject *gnomonProjectManager::createProject(const QString &path, const QS
     return d->project;
 }
 
+bool gnomonProjectManager::ExistingProject(const QString &path) const {
+    return gnomonProject::isDirAProject(QDir(QUrl(path).toLocalFile()));
+}
+
 void gnomonProjectManager::closeProject() {
     if(d->project) {
         d->project->close();
         delete d->project;
         d->project = nullptr;
     }
+}
+
+bool gnomonProjectManager::cleanProject(const QString &path) const {
+    auto info_dir_path = QUrl(path).toLocalFile() + "/.gnomon";
+    QDir info_dir(info_dir_path);
+    return info_dir.removeRecursively();
 }
 
 gnomonProjectManager *gnomonProjectManager::s_instance = nullptr;
--- src/gnomonProject/gnomonProjectManager.h
+++ src/gnomonProject/gnomonProjectManager.h
@@ -27,6 +27,8 @@ public:
                                              const QString &source);
     Q_INVOKABLE void closeProject();
     Q_INVOKABLE QVariantMap readProjectInfo(const QString &path);
+    Q_INVOKABLE bool ExistingProject(const QString &path) const;
+    Q_INVOKABLE bool cleanProject(const QString &path) const;
 
     gnomonProject *project(void);
 
