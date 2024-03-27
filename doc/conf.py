6f427362720193653899de6daf15fb9a27669bb2
Arthur Luciani <arthur.luciani@inria.fr>
2024-03-27 14:23:07 +0100

    bumping version to 1.0.0

--- doc/conf.py
+++ doc/conf.py
@@ -31,9 +31,9 @@ copyright = '2022, Inria'
 author = 'Mosaic project-team and AMDT team'
 
 # The short X.Y version
-version = '0.81'
+version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '0.81.1'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
--- doc_md/conf.py
+++ doc_md/conf.py
@@ -24,7 +24,7 @@ author = 'Mosaic project-team and AMDT team'
 # The short X.Y version
 version = '0.81'
 # The full version, including alpha/beta/rc tags
-release = '0.81.1'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
