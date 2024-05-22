5cd69aeb3f15632d47c85ede9c52a5e71afba85b
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2024-05-22 16:28:46 +0200

    add image to cell image filter workspace

--- src/gnomonWorkspace/gnomonWorkspaceCellImageFilter.h
+++ src/gnomonWorkspace/gnomonWorkspaceCellImageFilter.h
@@ -11,9 +11,9 @@ class GNOMONWORKSPACE_EXPORT gnomonWorkspaceCellImageFilter : public gnomonAlgor
 {
     Q_OBJECT
     Q_CLASSINFO("description", "\
-This workspace allows to apply filters on the cell image, producing a new output cell image.\n\
+This workspace allows to apply filters on a cell image, producing a new output cell image.\n\
 \n\
-Using the input cell image from the left view, a filter will be computed \
+Using the input cell image from the left view, a filtered image will be computed \
 and displayed in the right view.\n\
 \n")
 
